f59179ae1b0e9afb88bee64bd323130b00d016df
CodesWithRobi <rayercodes@gmail.com>
2024-01-17 15:05:30 +0530

    Added total number of passengers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let num = 0;
 const count = document.getElementById("count");
 const log = document.getElementById("log");
+const total = document.getElementById("total")
 
 function increment() {
     num++;
@@ -11,5 +12,10 @@ function increment() {
 function save() {
     log.innerText += `${num} + `;
     num = 0;
+    add()
+}
+
+function add() {
+    total.innerText = `Total : ${eval(log.innerText.slice(0 , -2))}`;
 }
 
